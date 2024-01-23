5345193bfa5a30dbd04e2d88830155b745922547
Martin Evans <martindevans@gmail.com>
2024-01-23 21:21:26 +0000

    optimised aysncify in build script

--- build.ps1
+++ build.ps1
@@ -4,7 +4,13 @@ cargo build --target $target --release
 
 $before = [math]::ceiling((Get-ChildItem ".\target\$target\release\demo_fleet.wasm").Length / 1kb)
 
-.\tools\binaryen\wasm-opt.exe ".\target\$target\release\demo_fleet.wasm" --output "output-$target.wasm" --strip-dwarf --asyncify --enable-bulk-memory --enable-simd -O4
+.\tools\binaryen\wasm-opt.exe `
+    ".\target\$target\release\demo_fleet.wasm" `
+    --output "output-$target.wasm" `
+    --strip-dwarf `
+    --asyncify --pass-arg=asyncify-imports@wasi_snapshot_preview1.sched_yield `
+    --enable-bulk-memory --enable-simd `
+    -O4
 
 $after = [math]::ceiling((Get-ChildItem "output-$target.wasm").Length / 1kb)
 
